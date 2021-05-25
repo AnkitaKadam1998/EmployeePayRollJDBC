81838fb46295681ca78f6851db45d6b4ab2f47fd
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-25 23:22:13 +0530

    ADD:ability to update basicsalary

--- EmployeePayroll.java
+++ EmployeePayroll.java
@@ -11,26 +11,26 @@ public class EmployeePayroll {
 	
 	public static void main(String args[]) throws ClassNotFoundException, SQLException
 	{
-	String url = "jdbc:mysql://localhost:3306/employee_payroll";
+	String url = "jdbc:mysql://localhost:3306/employee_payroll_service";
 	String userName = "root";
 	String password = "Ankita@1104";
 	
-	String query ="select * from employee";
-	
-	//String query = "update employee set Salary = '80000' where Name = 'Ankita'";
+	String query = "update employee_payroll set basic_pay = '50000' where Name = 'Ankita'";
 	
 	Class.forName("com.mysql.cj.jdbc.Driver");
 	Connection con = DriverManager.getConnection(url,userName,password);
 	Statement st = con.createStatement();
-	ResultSet rs = st.executeQuery(query);
-	//int rs = st.executeUpdate(query);
-	//System.out.println("Number of rows affected"+rs);
+	//ResultSet rs = st.executeQuery(query);
+	int rs = st.executeUpdate(query); 
+	System.out.println("Number of rows affected"+rs);
 	
-	String employeeData = "";
+	/* String employeeData = "";
 	while(rs.next()) {
-		employeeData = rs.getInt(1)+"-"+rs.getString(2)+"-"+rs.getString(3)+"-"+rs.getString(4)+"-"+rs.getString(5);		
+		employeeData = rs.getInt(1)+"-"+rs.getString(2)+"-"+rs.getString(3)+"-"+rs.getString(4)+"-"+rs.getString(5);
+				
 		System.out.println(employeeData);
-	} 
+	} */
+
 }
-}
+
 
