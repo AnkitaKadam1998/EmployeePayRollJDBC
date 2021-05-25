03b6e2965c67a9cd0bb9922781f098e964e23ba7
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-25 23:52:44 +0530

    ADD:ability to update basicsalary using preparedStatement

--- EmployeePayroll.java
+++ EmployeePayroll.java
@@ -2,34 +2,38 @@ package com.day34;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class EmployeePayroll {
-	
-	public static void main(String args[]) throws ClassNotFoundException, SQLException
-	{
-	String url = "jdbc:mysql://localhost:3306/employee_payroll_service";
-	String userName = "root";
-	String password = "Ankita@1104";
-	
-	String query = "update employee_payroll set basic_pay = '50000' where Name = 'Ankita'";
-	
-	Class.forName("com.mysql.cj.jdbc.Driver");
-	Connection con = DriverManager.getConnection(url,userName,password);
-	Statement st = con.createStatement();
-	//ResultSet rs = st.executeQuery(query);
-	int rs = st.executeUpdate(query); 
-	System.out.println("Number of rows affected"+rs);
-	
-	/* String employeeData = "";
-	while(rs.next()) {
-		employeeData = rs.getInt(1)+"-"+rs.getString(2)+"-"+rs.getString(3)+"-"+rs.getString(4)+"-"+rs.getString(5);
+	public static void main(String[] args) throws ClassNotFoundException, SQLException   {
+		//initializing url,username and password
+		String url = "jdbc:mysql://localhost:3306/employee_payroll_service";
+		String userName = "root";
+		String password = "Ankita@1104";
+		
+		int basic_pay = 70000;
+		String Name = "Manish";
+		String query = "update employee_payroll_service.employee_payroll set basic_pay =? where Name = ?";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		
+		Connection con = DriverManager.getConnection(url,userName,password);
+		
+		PreparedStatement st = con.prepareStatement(query);
+		st.setInt(1,basic_pay);
+		st.setString(2, Name);
+		
+		int count = st.executeUpdate();
 				
-		System.out.println(employeeData);
-	} */
+		System.out.println(count + "rows affected");
+		st.close();
+		con.close();
+		
+	}
 
 }
 
